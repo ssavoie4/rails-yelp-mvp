39529940ee052b07180ab2ad0512c4e9163ec584
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-01 11:02:35 -0400

    build action framework for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
