1fd53d67ef9b2788ded1ead545b187980f6e1b95
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-01 11:00:27 -0400

    build action framework for restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,19 @@
 class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :phone_number, :address, :category)
+  end
 end
