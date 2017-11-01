c46eccdaf42e0c0937ce2e70755cce7efbb2e7d5
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-01 11:18:57 -0400

    seeded the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "905-476-9287",
+    category:     "french"
+  },
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "905-476-9287",
+    category:     "italian"
+  },
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "905-476-9287",
+    category:     "japanese"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
