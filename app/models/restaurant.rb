80095c4a8e94983f9398e505ed6a100acbf60b5c
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-01 16:14:10 -0400

    style for restaurants#index and restaurants#show

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,4 +7,15 @@ class Restaurant < ApplicationRecord
   validates :address, presence: true
   validates :category, inclusion: { in: CATEGORY, allow_nil: false }
 
+  def avg_rating
+    # ratings = []
+    # self.reviews.each do |review|
+    #   ratings << review
+    # end
+    ratings = reviews.map do |review|
+      review.rating
+    end
+    ratings.reduce(:+)/ratings.size unless ratings.empty?
+  end
+
 end
